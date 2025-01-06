72b65d9cd7b0ceb0484298096ad9467dd616ea9d
sandip rai <iamsandip-git>
2025-01-06 16:36:07 +0545

    corected app.is and docker compose.yaml

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Chandan Tiwari!
+          Hello I am Sandip From Inspiring Lab!
         </a>
       </header>
     </div>
